0a74fc904d62cb3d809ec6590b9ef00d4c5990ba
PRoy <sam469roger@gmail.com>
2024-02-27 14:15:00 +0530

    added comments to index, secrets, .env.example files

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,43 @@
+// Import the express library and the Express type from the 'express' package
 import express, { Express } from 'express';
-import { PORT } from './secrets';
-import rootRouter from './routes';
-import { PrismaClient } from '@prisma/client';
-import { errorMiddleware } from './middlewares/errors';
+// Import the rateLimit function from the 'express-rate-limit' package
 import { rateLimit } from 'express-rate-limit';
+// Import the PrismaClient class from the '@prisma/client' package
+import { PrismaClient } from '@prisma/client';
+// Import the PORT constant from the 'secrets' file
+import { PORT } from './secrets';
+// Import the rootRouter from the 'routes' file
+import rootRouter from './routes';
+// Import the errorMiddleware function from the 'errors' middleware file
+import { errorMiddleware } from './middlewares/errors';
 
+// Create an express application
 const app: Express = express();
 
+// Create a rate limiter middleware with a window of 15 minutes and a maximum of 100 requests
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
 
+// Use the rate limiter middleware
 app.use(limiter);
 
+// Parse incoming JSON data
 app.use(express.json());
 
+// Mount the rootRouter under the '/api' path
 app.use('/api', rootRouter);
 
+// Create a new instance of the PrismaClient with query logging
 export const prismaClient = new PrismaClient({
   log: ['query'],
 });
 
+// Use the global error handling errorMiddleware
 app.use(errorMiddleware);
 
+// Start the server and listen on the specified PORT
 app.listen(PORT, () => {
   console.log(`Server is running at port:${PORT}`);
 });
--- src/secrets.ts
+++ src/secrets.ts
@@ -1,7 +1,11 @@
+// Import the 'dotenv' package
 import dotenv from 'dotenv';
 
+// Load environment variables from .env file
 dotenv.config({ path: '.env' });
 
+// Assign the value of the environment variable 'PORT' to the constant 'PORT'
 export const PORT = process.env.PORT;
 
+// Assign the value of the environment variable 'JWT_SECRET' to the constant 'JWT_SECRET'
 export const JWT_SECRET = process.env.JWT_SECRET!;
